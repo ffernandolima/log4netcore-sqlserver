cef8ffa73b2285427fd24ea5d2a7b27202380821
Fernando Luiz de Lima <fernando.lima@helloinnovation.com>
2019-08-27 12:25:03 -0300

    Configured nuget package Added a test project

---
+++ Log4NetCore.SqlServer.Tests/Log4NetCoreTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Log4NetCore.SqlServer.Tests
+{
+	[TestClass]
+	public class Log4NetCoreTests
+	{
+		[TestMethod]
+		public void GeneralTestMethod()
+		{
+		}
+	}
+}
